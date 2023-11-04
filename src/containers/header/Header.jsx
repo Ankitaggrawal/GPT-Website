ec20b2c65b7785fac1b7b491b0ae125445516bff
Ankitaggrawal <akaggrawal4252@gmail.com>
2023-11-04 16:31:26 +0530

    dc

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './header.css';
-import people from '../../assets/people.png';
+import peoples from '../../assets/peoples.png';
 import ai from '../../assets/ai.png'
 
 const Header = () => {
@@ -18,7 +18,7 @@ const Header = () => {
         </div>
 
         <div className="gpt3__header-content__people">
-          <img src={people} alt="people" />
+          <img src={peoples} alt="peoples" />
           <p>1,600 people requested access a visit in last 24 hours</p>
         </div>
       </div>
